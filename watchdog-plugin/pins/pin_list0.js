e2efe52ebddf1eaccdabcc8a5cd1316ae2a6df60
omotherhen <ngu.henry@hotmail.com>
2017-01-22 13:12:54 -0500

    last

--- watchdog-plugin/pins/pin_list0.js
+++ watchdog-plugin/pins/pin_list0.js
@@ -7,13 +7,13 @@ gpio.setup(7, gpio.DIR_OUT, write);
 gpio.setup(29, gpio.DIR_OUT, write);
 
 while (1) {
-	sleep.sleep(0.5)
+	sleep.sleep(1)
 	write(3)
-	sleep.sleep(0.5)
+	sleep.sleep(1)
 	write(5)
-	sleep.sleep(0.5)
+	sleep.sleep(1)
 	write(7)
-	sleep.sleep(0.5)
+	sleep.sleep(1)
 	write(29)
 }
 
@@ -22,12 +22,12 @@ function write(num) {
 		if (err) throw err;
 		console.log('Written to pin');
 	});
-	sleep.sleep(0.5)
+	sleep.sleep(1)
 	gpio.write(num, true, function (err) {
 		if (err) throw err;
 		console.log('Written to pin');
 	});
-	sleep.sleep(0.5)
+	sleep.sleep(1)
 	gpio.write(num, false, function (err) {
 		if (err) throw err;
 		console.log('Written to pin');
