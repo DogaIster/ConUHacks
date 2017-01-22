693322b7365ec17678fb3f0d36bf7f0e6659df9d
Doga Ister <DogaIster@Dogas-MacBook-Air.local>
2017-01-21 22:19:42 -0500

    spline chart

--- client2/dashboard.js
+++ client2/dashboard.js
@@ -14,18 +14,58 @@ $(window).ready(function() {
 	})
 
 	var chart = c3.generate({
+		size:{
+			height: 400,
+			width: 800
+		},
 		bindto: '#dashboard-charts',
 	    data: {
-	        json: [ data ],
 	        columns:[
-	            ['data']
+	            ['data1', 1, 2, 3, 4, 2, 10, 1],
+	            ['data2', 5,783,43678,4572432,1, 12, 4]
 	        ],
 	        keys: {
 	            value: up
 	        },
-	        type:'spline',
+	        type:'bar',
 
 	    },
+	    grid:{
+	    	x: {
+	    		show: true
+	    	},
+	    	y: {
+	    		show: true
+	    	},
+	    	focus: {
+	    		show: true
+	    	},
+	    	color: {
+	    		pattern: ['#4285F4', '#7CBB00']
+	    	},
+	    },
+	    axis:{
+	    	x: {
+	    		show: true,
+	    		label: {
+	    			text: 'uptime',
+	    			position: 'outer-right'
+	    		}
+	    	},
+	    	y: {
+	    		show: true,
+	    		label: {
+	    			text: 'last restart',
+	    			position: 'outer-top'
+	    		}
+	    	}
+	    },
+	    zoom: {
+	    	enabled: true
+	    },
+	    legend: {
+	    	show: true
+	    }
 	});
     }
   });
